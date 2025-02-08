c50b3afb9f759809c968cd93edc05cdf2e5a6fb9
agomizi1cm <77785410+agomizi1cm@users.noreply.github.com>
2025-02-08 20:14:24 +0900

    Update AlertBox.java

--- src/AlertBox.java
+++ src/AlertBox.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 public class AlertBox {
     public static void display(String title, String message) {
         Stage window = new Stage();
-        window.initModality(Modality.APPLICATION_MODAL); // 他のウィンドウ操作をブロック
+        window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
         window.setResizable(false);
         window.setMinWidth(300);
